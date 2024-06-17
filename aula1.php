77107bc838d7b3de59a0d079fa439052504d2e4a
03621983066 <03621983066@senacrs.edu.br>
2024-06-17 20:53:03 -0300

    second_class

---
+++ aula1.php
@@ -0,0 +1,15 @@
+<?php
+echo "aula1 <br>"; ;
+
+
+$cavalos = "15";
+$homens = "2050";
+$altura = "mauricio";
+$idade = 22;
+$logico = true;
+const $senai = "mecatronica";
+
+echo "eu dou a bunda para " . $homens . " homens e " .  $cavalos . " cavalos ";
+
+echo $altura . "<br> " . $idade
+?>
---
+++ operadoresAritmaticos.php
@@ -0,0 +1,26 @@
+<?php
+//operadores aritmaticos
+$a = 20;
+$b = 10;
+$soma;
+
+$soma = $a + $b;
+
+echo $soma . "<br>";
+
+$subtracao = $a - $b;
+
+echo $subtracao . "<br>";
+
+$multiplicacao = $a * $b;
+
+echo $multiplicacao. "<br>";
+
+$divisao = $a / $b;
+
+echo $divisao. "<br>";
+
+$resto = $a % $b;
+
+echo $resto. "<br>";
+?>
